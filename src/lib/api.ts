e5db1af1a1d030aed7bd802d24ce6f48b19d79f8
Aapka GitHub Username <satishkorat890@gmail.com>
2026-08-12 15:07:16 +0530

    added base url

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,3 +1,3 @@
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000";
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "https://my-next-js-restaurant-lnc6.vercel.app/";
 
 export default API_BASE_URL;
